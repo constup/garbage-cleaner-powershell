c40cdb527a94eabd42a87ef128ff3d1e9be93e82
constup <16638173+constup@users.noreply.github.com>
2024-11-28 03:41:35 +0100

    0.1.5
    QA:
    
    - Added tests for show-by-property

---
+++ tests/show-by-property.Tests.ps1
@@ -0,0 +1,45 @@
+Describe 'Show-Applications' {
+    BeforeAll {
+        . $PSScriptRoot/../src/show-by-property.ps1
+    }
+
+    It 'Should display a list of all applications' {
+        $expected = New-Object System.Collections.Generic.List[object]
+        $expected.AddRange(@("JetBrains", "Gradio", "WSL", "Viber", "Delphi", "Windows", ".NET", "Scoop"))
+
+        $result = Show-Applications -cleanupListFile $PSScriptRoot/cleanup-list.tests-sample.json
+        $result | Should -BeExactly $expected
+    }
+
+    It 'Should display a list of all categories' {
+        $expected = New-Object System.Collections.Generic.List[object]
+        $expected.AddRange(@("IDE", "AI", "Windows", "communication", ".NET", "installer"))
+
+        $result = Show-Categories -cleanupListFile $PSScriptRoot/cleanup-list.tests-sample.json
+        $result | Should -BeExactly $expected
+    }
+
+    It 'Should display a list of all entity categories' {
+        $expected = New-Object System.Collections.Generic.List[object]
+        $expected.AddRange(@("crash log", "temporary files", "log", "telemetry", "cache"))
+
+        $result = Show-EntityCategories -cleanupListFile $PSScriptRoot/cleanup-list.tests-sample.json
+        $result | Should -BeExactly $expected
+    }
+
+    It 'Should display a list of all types' {
+        $expected = New-Object System.Collections.Generic.List[object]
+        $expected.AddRange(@("file", "directory"))
+
+        $result = Show-Types -cleanupListFile $PSScriptRoot/cleanup-list.tests-sample.json
+        $result | Should -BeExactly $expected
+    }
+
+    It 'Should display a list of all custom categories' {
+        $expected = New-Object System.Collections.Generic.List[object]
+        $expected.AddRange(@())
+
+        $result = Show-CustomCategories -cleanupListFile $PSScriptRoot/cleanup-list.tests-sample.json
+        $result | Should -BeExactly $expected
+    }
+}
