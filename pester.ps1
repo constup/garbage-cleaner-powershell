5c7089ac7ac866cb8750c0be88349226e9bc29f2
constup <16638173+constup@users.noreply.github.com>
2025-01-18 02:41:59 +0100

    0.1.8
    Fixes:
    
    - Pester configuration

--- pester.ps1
+++ pester.ps1
@@ -1,7 +1,11 @@
 $config = New-PesterConfiguration
-$config.Run.Path = "./tests"
+$config.TestDrive.Enabled = $true
+$config.TestRegistry.Enabled = $true
+$config.Run.Path = @("./tests", "./tests/applications/steam")
+$config.CodeCoverage.RecursePaths = $true
 $config.CodeCoverage.Enabled = $true
 $config.CodeCoverage.Path = './src'
 $config.CodeCoverage.OutputPath = './coverage/coverage.xml'
+$config.CodeCoverage.OutputFormat = 'JaCoCo'
 
 Invoke-Pester -Configuration $config
